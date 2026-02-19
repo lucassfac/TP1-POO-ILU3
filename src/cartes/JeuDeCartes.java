4cb1c22cf5372fa96f07e97dac540040452098e7
lucassfac <lucas.de-oliveira-costa@univ-tlse3.fr>
2026-02-19 09:40:39 +0100

    Construction classes a corriger faire les methodes toString

--- src/cartes/JeuDeCartes.java
+++ src/cartes/JeuDeCartes.java
@@ -1,7 +1,13 @@
 package cartes;
 
 public class JeuDeCartes {
-	
+	private Configuration[] typeDeCartes = new Configuration[] {
+			new Configuration(new Borne(25), 10),
+			new Configuration(new Borne(50), 10),
+			new Configuration(Attaque(Type.FEU), 14),
+			
+			
+	};
 	
 	private class Configuration{
 		private Carte carte;
--- src/cartes/Type.java
+++ src/cartes/Type.java
@@ -1,8 +1,31 @@
 package cartes;
 
 public enum Type {
-	FEU,
-	ESSENCE,
-	CREVAISON,
-	ACCIDENT;
+	FEU("Feu Rouge","Feu Vert","Prioritaire"),
+	ESSENCE("Bideon d'essence","Panne d'essence","Citerne"),
+	CREVAISON("Roue de secours","Crevaison","Increvable"),
+	ACCIDENT("Réparation","Accident","As du volant");
+	
+	private final String nomAttaque;
+	private final String nomParade;
+	private final String nomBotte;
+	
+	Type(String nomAttaque, String nomParade, String nomBotte){
+		this.nomAttaque = nomAttaque;
+		this.nomParade = nomParade;
+		this.nomBotte = nomBotte;
+	}
+
+	public String getNomAttaque() {
+		return nomAttaque;
+	}
+
+	public String getNomParade() {
+		return nomParade;
+	}
+
+	public String getNomBotte() {
+		return nomBotte;
+	}
+	
 }
